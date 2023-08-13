4aebbec65114b746eddfa0ef84dff4352a64318d
beckguo <eaouyuan@gmail.com>
2023-08-13 21:59:44 +0800

    10-1 finish

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
 import "./index.css";
+
+// const btn1=document.getElementById("btn")
+// const text1=document.getElementById("text")
+// const box1=document.getElementById("box")
+// const boxUl1=document.getElementById("boxUl")
+
+// btn1.addEventListener("click",function(){
+//     console.log('asdfasdfasdf')
+//     text1.classList.remove('text-fb')
+//     text1.classList.add('text-fb')
+//     // box1.innerHTML="<p>abc</p>"
+//     // boxUl1.innerHTML=`
+//     //     <ul>
+//     //         <li></li>
+//     //         <li></li>
+//     //         <li></li>
+//     //         <li></li>
+//     //         <li></li>
+//     //     </ul>
+//     // `
+//     let str=""
+//     const li=`<li>test</li>`
+//     for (let i=0; i<5; i++) {
+//         str=str+li;
+//     }
+
+//     boxUl1.innerHTML=str
+
+// })
